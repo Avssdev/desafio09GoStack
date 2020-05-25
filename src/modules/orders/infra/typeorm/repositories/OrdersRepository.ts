d3b9dc402fb4327f509a9568beac57269cbc1c0d
Alvaro S <avssdev.development@gmail.com>
2020-05-25 19:21:13 -0300

    Try solve error 05_25_2020

--- src/modules/orders/infra/typeorm/repositories/OrdersRepository.ts
+++ src/modules/orders/infra/typeorm/repositories/OrdersRepository.ts
@@ -23,7 +23,7 @@ class OrdersRepository implements IOrdersRepository {
   }
 
   public async findById(id: string): Promise<Order | undefined> {
-    const order = this.ormRepository.findOne({
+    const order = await this.ormRepository.findOne({
       where: {
         id,
       },
